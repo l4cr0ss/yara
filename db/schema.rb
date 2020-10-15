5616f21097deafdb184833b30b7a8a391969eddb
Jefferson Hudson <jefferson.hudson@gmail.com>
2020-10-15 11:03:03 -0400

    Add members

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_15_144911) do
+ActiveRecord::Schema.define(version: 2020_10_15_150249) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "members", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Article.create([
+  {title: 'My first post', content: 'This is my first post on this new application. I really like the interface! - Freddy'},
+  {title: 'All about NodeJS', content: 'Longform content about how NodeJS works will go - Abi'}
+])
+
+Member.create([
+  {name: 'Freddy', email: 'freddy@example.com'},
+  {name: 'Abi', email: 'abi@example.com'}
+])
